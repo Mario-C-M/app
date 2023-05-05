da3066ce01a8c60c2ee2d87285992ee16db9f298
Mario-C-M <mario.cid.m@hotmail.com>
2023-05-05 12:09:31 +0200

    acciones botón

--- secciones/cursos2.php
+++ secciones/cursos2.php
@@ -8,25 +8,20 @@ $conexionBD=BD::crearInstancia();
 $id=isset($_POST['id'])?$_POST['id']:'';
 // igual para el nombre del curso
 $nombre_curso=isset($_POST['nombre_curso'])?$_POST['nombre_curso']:'';
-//identificar la acción del usuario
-$accion= isset($_POST['accion'])?$_POST['accion']:''; //Evalúa el contenido por medio del isset y qué acción realiza el usuario
-if ($accion!=''){ //si la acción es diferente de vacío
-    switch ($accion) { //evalúa la acción
-        case 'agregar': //realiza un INSERT
+// igual para la acción
+$accion=isset($_POST['accion'])?$_POST['accion']:'';
+//identificar la acción
+if ($accion) {
+    switch ($accion) {
+        case 'agregar':
             $sql="INSERT INTO cursos(id, nombre_curso) VALUES(NULL, '$nombre_curso')";
-            echo $sql;
-            break;
-            case 'editar': //realiza un UPDATE
-            $sql="UPDATE cursos SET nombre_curso='$nombre_curso' WHERE id='$id'";
-            echo $sql;
-            break;
-            case 'borrar': //realiza un DELETE
-            $sql="DELETE FROM cursos WHERE id='$id'";
-            echo $sql;
+            $conexionBD->ejecutarConsulta($sql);
             break;
     }
+    
 }
-//se crea la consulta
+
+
 $consulta=$conexionBD->prepare("SELECT * FROM cursos"); //consulta toda la información de la tabla cursos
 $consulta->execute();//lo ejecuta
 $listaCursos=$consulta->fetchAll();//lo guarda en el array
