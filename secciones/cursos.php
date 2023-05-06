b8130ed118b8e4cb95846089ec6a1ddaa053d910
Mario-C-M <mario.cid.m@hotmail.com>
2023-05-06 21:39:16 +0200

    Falta inicio sesión

--- secciones/cursos.php
+++ secciones/cursos.php
@@ -11,9 +11,9 @@ $nombre_curso=isset($_POST['nombre_curso'])?$_POST['nombre_curso']:'';
 //identificar la acción del usuario
 $accion= isset($_POST['accion'])?$_POST['accion']:''; //Evalúa el contenido por medio del isset y qué acción realiza el usuario
 
-print_r($_POST);
+// print_r($_POST);
 
-if ($accion!=''){ //si la acción es diferente de vacío
+if($accion!=''){ //si la acción es diferente de vacío
     switch ($accion) { //evalúa la acción
         
             case 'agregar': //realiza un INSERT
@@ -29,7 +29,7 @@ if ($accion!=''){ //si la acción es diferente de vacío
             $consulta->bindParam(':id',$id); //pasamos el parámetro se reemplaza id en este parametro
             $consulta->bindParam(':nombre_curso',$nombre_curso); //pasamos el parámetro se reemplaza nombre_curso en este parametro
             $consulta->execute(); //ejecutamos
-            echo $sql;
+            // echo $sql;
             break;
 
             case 'borrar': //realiza un DELETE
@@ -37,7 +37,7 @@ if ($accion!=''){ //si la acción es diferente de vacío
             $consulta=$conexionBD->prepare($sql); //prepara la consulta
             $consulta->bindParam(':id',$id); //pasamos el parámetro
             $consulta->execute(); //ejecutamos
-            echo $sql;
+            // echo $sql;
             break;
             
             case 'Seleccionar': //seleccionamos el recurso
@@ -48,7 +48,7 @@ if ($accion!=''){ //si la acción es diferente de vacío
             $curso=$consulta->fetch(PDO::FETCH_ASSOC);//obtiene la información con fetch_assoc de todos los recursos recuperados del SELECT id
             // print_r($curso); //se imprime el array. se utilizó para la prueba de funcionamiento
             $nombre_curso=$curso['nombre_curso']; //se recupera la información de la consulta sql SELECT
-            echo $nombre_curso; //se imprime el valor por pantalla
+            // echo $nombre_curso; //se imprime el valor por pantalla
             break;
     }
 }
--- secciones/vista_alumnos.php
+++ secciones/vista_alumnos.php
@@ -1,6 +1,6 @@
 <?php include('../templates/cabecera.php'); ?>
 <?php include('../secciones/alumnos.php'); ?>
-
+<br/>
 <div class="row">
     <div class="col-5">
         <form action="" method="post">
@@ -9,86 +9,115 @@
                 Alumnos
             </div>
             <div class="card-body">
-                    
-                    <div class="mb-3">
-                    <label for="" class="form-label">ID</label>
-                    <input type="text"
-                        class="form-control" 
-                        name="id" id="id" 
-                        aria-describedby="helpId" 
-                        placeholder="id">
-                    </div>
+            
+            <div class="mb-3 d-none">
+              <label for="id" class="form-label">ID</label>
+              <input type="text"
+                class="form-control" name="id" value="<?php echo $id;?>" id="id" aria-describedby="helpId" placeholder="ID">
+            </div>
+            <div class="mb-3">
+              <label for="nombre" class="form-label">Nombre</label>
+              <input type="text"
+                class="form-control" name="nombre"  value="<?php echo $nombre;?>" id="nombre" aria-describedby="helpId" placeholder="Escriba el nombre">
+            </div>
 
-                    <div class="mb-3">
-                    <label for="" class="form-label">Nombre</label>
-                    <input type="text"
-                        class="form-control" 
-                        name="nombre" id="nombre" 
-                        aria-describedby="helpId" 
-                        placeholder="Escribe tu nombre">
-                    </div>
+            <div class="mb-3">
+              <label for="apellidos" class="form-label">Apellidos</label>
+              <input type="text"
+                class="form-control" name="apellidos"  value="<?php echo $apellidos;?>" id="apellidos" aria-describedby="helpId" placeholder="Escriba los apellidos">
+              
+            </div>
+            
+            <div class="mb-3">
+              <label for="" class="form-label">Cursos del alumno:</label>
 
-                <div class="mb-3">
-                <label for="" class="form-label">Apellidos</label>
-                <input type="text"
-                    class="form-control" 
-                    name="apellidos" 
-                    id="apellidos" 
-                    aria-describedby="helpId" 
-                    placeholder="Escribe tus apellidos">
-                </div>
+              <select multiple class="form-control" name="cursos[]" id="listaCursos">
+                
+              <?php foreach($cursos as $curso){ ?>
+                
+                
+                    <option
+                    <?php 
+                        if(!empty($arregloCursos)):
+                            if(in_array($curso['id'],$arregloCursos)):
+                                echo 'selected';
+                            endif;
+                        endif;
+                    ?> 
+                    value="<?php echo $curso['id'];?>" >
+                    <?php echo $curso['id'];?> - <?php echo $curso['nombre_curso'];?>
+                    </option>
 
-                <div class="mb-3">
-                    <label for="" class="form-label">Cursando</label>
-                    <select multiple class="form-select form-select-lg" name="cursos[]" id="listaCursos">
-                        <option selected>Selecciona una opción</option>
-                        <option value="">Curso 1</option>
-                        <option value="">Curso 2</option>
-                    </select>
-                </div>
+              <?php  } ?>
 
-                <div class="btn-group" role="group" aria-label="Button group name">
-                    <button type="submit" class="btn btn-primary">Agregar</button>
-                    <button type="submit" class="btn btn-secondary">Editar</button>
-                    <button type="submit" class="btn btn-dark">Borrar</button>
-                </div>
+              </select>
 
             </div>
 
-    </form>
+            <div class="btn-group" role="group" aria-label="">
+                <button type="submit" name="accion" value="agregar" class="btn btn-primary">Agregar</button>
+                <button type="submit" name="accion" value="editar"class="btn btn-secondary">Editar</button>
+                <button type="submit" name="accion" value="borrar" class="btn btn-dark">Borrar</button>
+            </div>
 
+
+            </div>
+            
+        </div>
+
+
+        </form>
     </div>
-</div>
     <div class="col-7">
-
-        <div class="table-responsive">
-            <table class="table table-info">
+            <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Acciones</th>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="">
-                        <td scope="row">R1C1</td>
-                        <td>R1C2</td>
-                        <td>R1C3</td>
+                  
+                <?php foreach($alumnos as $alumno): ?>
+                    <tr>
+                        <td><?php echo $alumno['id'];?></td> 
+
+                        <td> 
+                            <?php echo $alumno['nombre'];?> <?php echo $alumno['apellidos'];?> 
+                            
+                            <br/>
+                            <?php foreach($alumno["cursos"] as $curso){ ?> 
+                                    - <a href="certificado.php?idcurso=<?php echo $curso['id']; ?>&idalumno=<?php echo $alumno["id"];?>"> 
+                                    <i class="bi bi-filetype-pdf text-danger"></i> <?php echo $curso['nombre_curso']; ?> 
+                                </a><br/>
+                                
+
+                            <?php  } ?>
+                        
+                        </td>
+
+
+                        <td>
+                        <form action="" method="post">
+
+                            <input type="hidden" name="id" value="<?php echo $alumno['id'];?>">
+                            <input type="submit" value="Seleccionar" name="accion" class="btn btn-info">
+                        </form>
+
+
+                        </td>
                     </tr>
-                    <tr class="">
-                        <td scope="row">Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                    </tr>
+                  <?php endforeach; ?>
+
                 </tbody>
             </table>
-        </div>
-        
-
+            
     </div>
-
 </div>
 
-<?php include('../templates/pie.php'); 
-?>
+
+
+
+
+<?php include('../templates/pie.php'); ?>
